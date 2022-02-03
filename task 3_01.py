d295600ce4b652e5b8c31c11165aae374d7e1d22
Sergei Bykov <ss.bykoff@gmail.com>
2022-02-03 15:51:07 +0300

    Поправил слеши

--- task 3_01.py
+++ task 3_01.py
@@ -6,11 +6,11 @@ def txt_file_analysis():
     list_files = []
     list_txt_files = []
     for file_name in file_list_name:
-        with open(str(file_path) + '\\' + file_name, 'r', encoding='UTF-8') as txt_file:
+        with open(str(file_path) + '/' + file_name, 'r', encoding='UTF-8') as txt_file:
             list_txt_files = txt_file.readlines()
             list_files.append([str(len(list_txt_files)), file_name, list_txt_files])
     list_files.sort()
-    with open(str(file_path) + '\\result.txt', 'w', encoding='UTF-8') as result:
+    with open(str(file_path) + '/result.txt', 'w', encoding='UTF-8') as result:
         for file in list_files:
             result.write(file[1] + '\n')
             result.write(file[0] + '\n')
