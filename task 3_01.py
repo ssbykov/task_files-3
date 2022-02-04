1a4b997fa14c434c9e992494a1dceb50d3cd7ddf
Sergei Bykov <ss.bykoff@gmail.com>
2022-02-04 12:15:10 +0300

    поменял названия переменных

--- task 3_01.py
+++ task 3_01.py
@@ -3,15 +3,15 @@ from pathlib import Path
 def txt_file_analysis():
     file_path = Path(__file__).parent.absolute()
     file_list_name = ['1.txt', '2.txt', '3.txt']
-    list_files = []
-    list_txt_files = []
+    common_txt_file_list = []
+    txt_file_list = []
     for file_name in file_list_name:
         with open(str(file_path) + '/' + file_name, 'r', encoding='UTF-8') as txt_file:
-            list_txt_files = txt_file.readlines()
-        list_files.append([str(len(list_txt_files)), file_name, list_txt_files])
-    list_files.sort()
+            txt_file_list = txt_file.readlines()
+        common_txt_file_list.append([str(len(txt_file_list)), file_name, txt_file_list])
+    common_txt_file_list.sort()
     with open(str(file_path) + '/result.txt', 'w', encoding='UTF-8') as result:
-        for file in list_files:
+        for file in common_txt_file_list:
             result.write(file[1] + '\n')
             result.write(file[0] + '\n')
             result.write(''.join(file[2]) + '\n')
--- task 3_02.py
+++ task 3_02.py
@@ -5,15 +5,15 @@ def txt_file_analysis():
     if list(file_path.glob('result.txt')):
         Path(str(file_path) + '/result.txt').unlink()
     text_files = file_path.glob('*.txt')
-    list_files = []
-    list_txt_files = []
+    common_txt_file_list = []
+    txt_file_list = []
     for txt_file_path in text_files:
         with open(txt_file_path, 'r', encoding='UTF-8') as txt_file:
-            list_txt_files = txt_file.readlines()
-        list_files.append([str(len(list_txt_files)), txt_file_path.name, list_txt_files])
-    list_files.sort()
+            txt_file_list = txt_file.readlines()
+        common_txt_file_list.append([str(len(txt_file_list)), txt_file_path.name, txt_file_list])
+    common_txt_file_list.sort()
     with open(str(file_path) + '/result.txt', 'w', encoding='UTF-8') as result:
-        for file in list_files:
+        for file in common_txt_file_list:
             result.write(file[1] + '\n')
             result.write(file[0] + '\n')
             result.write(''.join(file[2]) + '\n')
